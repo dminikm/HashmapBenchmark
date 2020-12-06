12e912f1323c8b2d31b8da562e1e5b320dc27607
Dminik <dminik.m@email.cz>
2020-12-06 18:30:08 +0100

    Added data generator for the second test (Hash-join)

---
+++ data/create_hash_join_data.py
@@ -0,0 +1,55 @@
+import random
+import sys
+
+random.seed('mec0037')
+
+input_file_name = sys.argv[1] or 'test.ft.txt.out'
+num_smaller_entries = int(sys.argv[2]) or 100000
+num_larger_entries = int(sys.argv[2]) or 1000000
+
+text = ''
+with open(input_file_name) as file:
+    text = file.read().replace('\n', '').replace('\r\n', '')
+
+def prepare_smaller_entries(text, num_entries, file_name):
+    ids = []
+    num_id = 1
+
+    with open(file_name, mode = 'w') as file:
+        for i in range(num_entries):
+            new_id = num_id
+            ids.append(new_id)
+
+            # Simulate deletions
+            if random.choices([0, 1], weights=(90, 10), k=1)[0] == 1:
+                num_id = num_id + random.randint(1, 30)
+
+            str_len = random.randint(20, 80)
+            str_start = random.randint(0, len(text) - str_len)
+
+            file.write(f"{new_id};{text[str_start:str_start + str_len]}\n")
+            num_id += 1
+
+    return ids
+
+def prepare_larger_entries(text, ids, num_entries, file_name):
+    num_id = 1
+
+    with open(file_name, mode = 'w') as file:
+        for i in range(num_entries):
+            new_id = num_id
+
+            # Simulate deletions
+            if random.choices([0, 1], weights=(90, 10), k=1)[0] == 1:
+                num_id = num_id + random.randint(1, 30)
+
+            str_len = random.randint(20, 80)
+            str_start = random.randint(0, len(text) - str_len)
+
+            join_id = random.choice(ids)
+
+            file.write(f"{new_id};{join_id};{text[str_start:str_start + str_len]}\n")
+            num_id += 1
+
+prepared_ids = prepare_smaller_entries(text, num_smaller_entries, 'data/hash_join_smaller.txt')
+prepare_larger_entries(text, prepared_ids, num_larger_entries, 'data/hash_join_larger.txt')
